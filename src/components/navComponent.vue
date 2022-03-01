7d0c23efa3e2bc2d6bf34fe5d75a2ad49f1941a2
Fede <fedeeblengio@gmail.com>
2022-02-28 22:35:26 -0300

    save user info into db

--- src/components/navComponent.vue
+++ src/components/navComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container px-4 px-lg-5">
-        <a class="navbar-brand color-white">Zaluda</a>
+        <a class="navbar-brand" href="/home">Zaluda</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -19,8 +19,21 @@
           id="navbarSupportedContent"
         >
           <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+            <li>
+              <a class="nav-link active" aria-current="page" v-if="isLogin"
+                >Welcome, {{ userInfo.tf }}</a
+              >
+            </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page">Home</a>
+              <a class="nav-link active" aria-current="page" href="/home"
+                >Home</a
+              >
+            </li>
+            <li>
+              <a class="nav-link active" aria-current="page">
+                <button @click="login()" v-if="!userInfo">Login</button>
+                <button @click="logOut()" v-else>Sign out</button>
+              </a>
             </li>
           </ul>
         </div>
@@ -29,6 +42,47 @@
   </div>
 </template>
 <script>
+import { global } from "../global";
+import axios from "axios";
+export default {
+  name: "navComponent",
+  data() {
+    return {
+      isLogin: false,
+      userInfo: "",
+    };
+  },
+  methods: {
+    async logOut() {
+      const result = await this.$gAuth.signOut();
+      this.isLogin = false;
+      this.userInfo = "";
+    },
+    async login() {
+      const googleUser = await this.$gAuth.signIn();
+      this.userInfo = googleUser.getBasicProfile();
+      this.isLogin = this.$gAuth.isAuthorized;
+      this.saveInfoUsers();
+    },
+    saveInfoUsers() {
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      let data = {
+        id_google: this.userInfo.TW,
+        full_name: this.userInfo.tf,
+        mail: this.userInfo.zv,
+      };
+      axios.post(global.url + "users", data, config).then((res) => {
+        if (res.status == 200) {
+          console.log("bien");
+        }
+      });
+    },
+  },
+};
 </script>
 <style>
 @import "@/assets/base.css";
--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-import Vue from 'vue'
-import VueCompositionAPI, { createApp, h } from '@vue/composition-api'
-
 import App from './App.vue'
-import router from './router'
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueCompositionAPI, { createApp, h } from '@vue/composition-api'
 
 Vue.use(VueCompositionAPI)
 
+const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '*', redirect: '/home' },
+
+];
+
+const router = new VueRouter({ routes, mode: 'history' });
 const app = createApp({
   router,
   render: () => h(App)
 })
 
+
 app.mount('#app')
+
+
+import GAuth from 'vue-google-oauth2'
+const gauthOption = {
+  clientId: '918385212039-sqqqgs3tr4jtn744kghddi2u12b6hdt2.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'consent',
+  fetch_basic_profile: true
+}
+Vue.use(GAuth, gauthOption)
