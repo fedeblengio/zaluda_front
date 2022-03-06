01a758e3ab5554c2096d3a8a430bebe8196e58fa
Fede <fedeeblengio@gmail.com>
2022-03-06 20:42:29 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+     integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"> 
+     
     <vue-headful :title="title" />
     <navComponent></navComponent>
     <headerComponent></headerComponent>
